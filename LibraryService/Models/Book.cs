173ae6d21eb2d6400c5448fca85f1062ac546ae7
donal cameron <donal.s.cameron@gmail.com>
2019-03-21 12:48:26 +0000

    inheritance

--- LibraryService/Models/Book.cs
+++ LibraryService/Models/Book.cs
@@ -6,22 +6,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace LibraryService.Models
 {
-    public class Book
+    public class Book: LibraryItem
     {
-        public int CatalogueId { get; set; }
+        public int Id { get; set; }
         [Key]
-        public string Title { get; set; }
+
         public string Author { get; set; }
-        public string Genre { get; set; }
+        
         public string Publisher { get; set; }
-        public int LibraryId { get; set; }
-        public string Status { get; set; }
-        public int UserId { get; set; }
-        public string AgeRestriction { get; set; }
-        public int Pages { get; set; }
-        public int PurchaseValue { get; set; }
-        public Nullable<DateTime> ReturnDate { get; set; }
-        public Nullable<DateTime> DateAdded { get; set; }
+        
+        
+        
+       
+       public int Pages { get; set; }
+        
+       
+        
 
         public virtual Library Library { get; set; }
         public virtual User User { get; set; }
---
+++ LibraryService/Models/LibraryItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryService.Models
+{
+    public abstract class LibraryItem
+    {
+   
+    
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int LibraryId { get; set; }
+        public string Status { get; set; }
+        public int UserId { get; set; }
+        public string AgeRestriction { get; set; }
+        public int PurchaseValue { get; set; }
+        public Nullable<DateTime> ReturnDate { get; set; }
+        public Nullable<DateTime> DateAdded { get; set; }
+
+    }
+}
