783221b2294eeb6178e3d543d6cbf7c22d12f252
b2023507 <b2023507@VSTEACH-L-TS5>
2019-04-23 15:41:16 +0100

    commented out line of code which was preventing login

--- LibraryService/Startup.cs
+++ LibraryService/Startup.cs
@@ -16,7 +16,7 @@ namespace LibraryService
             app.UseHangfireDashboard();
             app.UseHangfireServer();
 
-            RecurringJob.AddOrUpdate(() => new Services.Service.LibraryItemService().UpdateStatus(), Cron.Daily);
+            //RecurringJob.AddOrUpdate(() => new Services.Service.LibraryItemService().UpdateStatus(), Cron.Daily);
         }
     }
 }
