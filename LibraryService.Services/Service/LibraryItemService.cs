757dcd81edc4a21fa9a3ca869b1d317db06bcc32
Leonie <Leonie@DESKTOP-S0G8OTB>
2019-04-25 18:11:01 +0100

    implemented mail service

--- LibraryService.Services/Service/LibraryItemService.cs
+++ LibraryService.Services/Service/LibraryItemService.cs
@@ -6,6 +6,8 @@ using LibraryService.Services.IService;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -50,6 +52,48 @@ namespace LibraryService.Services.Service
                             }
                         }
                     }
+                }
+            }
+        }
+
+        public void SendOverdueMail()
+        {
+            SmtpClient client = new SmtpClient("smtp.googlemail.com");
+            client.UseDefaultCredentials = false;
+            client.EnableSsl = true;
+            client.Port = 587;
+            client.Credentials = new NetworkCredential("LibraryServiceSheffield@gmail.com", "LibraryService123");
+
+            foreach (User user in _userDAO.GetUsers())
+            {
+                foreach(KeyValuePair<Book, DateTime> book_duedate in user.LoanedBooks)
+                {
+                    if (book_duedate.Value.AddDays(1).CompareTo(DateTime.Today) <= 0)
+                    {
+                        MailMessage mailMessage = new MailMessage();
+                        mailMessage.From = new MailAddress("LibraryServiceSheffield@gmail.com");
+                        mailMessage.To.Add(user.UserName);
+                        mailMessage.Subject = "Hello There";
+                        mailMessage.Body = "Hello my friend!";
+
+                        client.Send(mailMessage);
+                    }
+
+                }
+
+                foreach (KeyValuePair<DVD, DateTime> dvd_overdue in user.LoanedDVDs)
+                {
+                    if (dvd_overdue.Value.AddDays(1).CompareTo(DateTime.Today) <= 0)
+                    {
+                        MailMessage mailMessage = new MailMessage();
+                        mailMessage.From = new MailAddress("LibraryServiceSheffield@gmail.com");
+                        mailMessage.To.Add(user.UserName);
+                        mailMessage.Subject = "Hello There";
+                        mailMessage.Body = "Hello my friend!";
+
+                        client.Send(mailMessage);
+                    }
+
                 }
             }
         }
